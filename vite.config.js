8653e294bbb6bb10cc26371f34cb9f115a2f4766
prem-jeet <jeet4300795@gmail.com>
2022-08-11 10:39:44 +0530

    initial

---
+++ vite.config.js
@@ -0,0 +1,27 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    port: 3000,
+    open: true,
+    fs: {
+      strict: false,
+    },
+  },
+  resolve: {
+    alias: [
+      {
+        find: "@",
+        replacement: path.resolve(__dirname, "./src"),
+      },
+      {
+        find: /^~.+/,
+        replacement: (val) => val.replace(/^~/, ""),
+      },
+    ],
+  },
+});
